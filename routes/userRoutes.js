b2ff5fa148d4d97d7a4140edc85bb4fe0d8772c4
mingyang <limingyanglmy10@gmail.com>
2024-07-25 00:13:03 +0800

    added forgot password webpage

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,14 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+router.get('/resetPassword/:token', (req, res) => {
+  res.status(200).render('passwordResetWebpage', {
+    token: req.params.token
+  });
+});
+
 router
   .route('/')
   .get(userController.getAllUsers)
